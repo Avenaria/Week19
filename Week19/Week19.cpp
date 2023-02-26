66e27b8ae7320e153659b4358719ae3857779ab7
1 <1@LAPTOP-8U2CR0NO>
2023-02-26 18:16:49 +0300

    Добавьте файлы проекта.

---
+++ Week19/Week19.cpp
@@ -0,0 +1,32 @@
+﻿#include"library.cpp"
+#include "library.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+int main()
+{
+    Library l;
+    l.PrintBooks();
+    l.EditBook(3);
+    l.FindName("name4");
+    l.FindAuthor("author4");
+
+    l.SortName();
+    l.PrintBooks();
+
+    l.SortAuthor();
+    l.PrintBooks();
+
+    l.SortPublish();
+    l.PrintBooks();
+
+   
+
+}
+
